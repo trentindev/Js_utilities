c4c34255425a1107bfe6e9bdee93531170249de9
Laurent Dislaire <siladire@free.fr>
2024-01-08 12:13:11 +0100

    doc for randomHexColor()

--- utilities.js
+++ utilities.js
@@ -62,16 +62,24 @@ export const titleize = (title, lang = 'fr') => {
  * @param {*} str 
  * @returns str
  */
-export const toCamelCase = (str) => str.trim().replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''));
+export const toCamelCase = (str) => str.trim().replace(/[-_\s]+(.)?/g, (_, c) => (c ? c.toUpperCase() : ''))
 
+
+/**
+ * @description  Convert a String in Snake Case to Camel Case
+ * @param {*} str 
+ * @returns str
+ */
+export const snakeToCamelCase = (s) => s.toLowerCase().replace(/(_\w)/g, (w) => w.toUpperCase().substr(1))
 
 
 /**
  * @description The function takes a string as input, splits it into an array of characters, reverses the order of
- * the characters, and then joins them back together into a new string.
+ * the characters, and then joins them back together into a new string.  Immutable : Do not modify the str variable
  * @param str - The input string that needs to be reversed.
+ * @returns new str 
  */
-export const reverseString = (str) => str.split('').reverse().join('');
+export const reverseString = (str) => str.split('').reverse().join('')
 
 
 //////////////////////////////  Iterables   //////////////////////////////////////////////////////////
@@ -89,7 +97,7 @@ export const removeDuplicates = (arr) => [...new Set(arr)]
  * @param {*} arr
  * @return {*} 
  */
-const last=(arr) => {
+export const last=(arr) => {
   return typeof arr == 'object' ? arr[arr.length-1] : false
 }
 
@@ -99,9 +107,17 @@ const last=(arr) => {
  * @param {*} arr
  * @return {*} 
  */
-const first=(arr) => {
+export const first=(arr) => {
   return typeof arr == 'object' ? arr[0] : false
 }
+/**
+ * Ensurie that two arrays contain the same elements (in any order) 
+ * and that these elements occur the same number of times in both arrays. *
+ * @param {*} arr1
+ * @param {*} arr2
+ */
+export const areEqual = (arr1, arr2) =>
+  arr1.sort().join(',') === arr2.sort().join(',');
 
 
 
@@ -115,19 +131,57 @@ const first=(arr) => {
  * @param int - The number to be converted to a roman numeral
  * @returns The roman numeral equivalent of the number passed in.
  */
-function parseToRoman(int) {
+export const parseToRoman= (int)=> {
   if (int === 0) return "zero does not exist in the Roman numeral system"
   if (int < 0) return "No negative number conversion"
   if (!Number.isInteger(int)) return "The number must be an integer"
   
-  var roman = "";
-  var romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
-  var decimal =      [1000, 900, 500, 400, 100,   90,  50,   40,  10,     9,   5,    4,  1 ];
+  let roman = ""
+  let romanNumeral = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+  let decimal =      [1000, 900, 500, 400, 100,   90,  50,   40,  10,     9,   5,    4,  1 ]
+
   for (let i = 0; i < decimal.length; i++) {
     while (decimal[i] <= int) {
-      roman += romanNumeral[i];
-      int -= decimal[i];
+      roman += romanNumeral[i]
+      int -= decimal[i]
     }
   }
-  return roman;
+  return roman
 }
+
+/**
+ * convert a Map object to a JSON string without losing any information:
+ * * @param {*} map
+ */
+export const mapToJson = (map) => JSON.stringify(Object.fromEntries(map))
+
+/**
+ * convert a JSON object to a Map object :
+ * * @param {*} json
+ */
+export const jsonToMap = (json) => new Map(Object.entries(JSON.parse(json)))
+
+
+//////////////////////////////////// random generation ////////////////////////////////////////
+
+/**
+ * The function generates a random hexadecimal color code by generating a random decimal number,
+ * converting it to hexadecimal,
+ * and then formatting it to ensure it has 6 characters with trailing zeros if necessary.
+ * @return str 
+ */
+export const randomHexColor = () => `#${Math.random().toString(16).slice(2, 8).padEnd(6, '0')}`
+
+/**
+ * Generate a Random UUID
+ * UUID is an acronym for University Unique Identifier. 
+ * UUID is a 128-bit value that uniquely identifies an object or entity on the internet.
+ * @param {*} a
+ */
+export const generateRandomUUID = (a) =>
+  a
+    ? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+    : ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
+        /[018]/g,
+        generateRandomUUID
+      );
